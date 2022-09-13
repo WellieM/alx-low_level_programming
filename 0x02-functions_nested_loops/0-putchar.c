c05456c7828a651ed787a04be7230c7c2bfde08a
WellieM <wellingtonmuhura@gmail.com>
2022-09-13 03:49:14 -0700

    Putcharrr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include "school.h"	
+
+	
+
+	/**
+	 *
+	 * 	* main - function to print
+	 *
+	 * 		*
+	 *
+	 * 			* Return: 0
+	 *
+	 * 				*/
+
+	int main(void)
+
+		{
+
+				char school[9] = {'s', 'c', 'h', 'o', 'o', 'l'};
+
+					unsigned int c;
+
+						
+
+						for (c = 0; c < sizeof(school); c++)
+
+								{
+
+										_putchar(school[c]);
+
+											}
+
+							_putchar('\n');
+
+								return (0);
+
+									}
+
+
